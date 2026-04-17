496c7a902d16b5ecd759a8a226a0e7b2a538f1b3
Olga Margevich <bandage92@bk.ru>
2026-04-17 08:28:35 -0400

    update gitignore

---
+++ target/gatling/steploadtest-20260417112543086/js/stats.js
@@ -0,0 +1,703 @@
+var stats = {
+    type: "GROUP",
+name: "All Requests",
+path: "",
+pathFormatted: "group_missing-name--1146707516",
+stats: {
+    "name": "All Requests",
+    "numberOfRequests": {
+        "total": "6523",
+        "ok": "6523",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "94",
+        "ok": "94",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "9255",
+        "ok": "9255",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "154",
+        "ok": "154",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "170",
+        "ok": "170",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "131",
+        "ok": "131",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "167",
+        "ok": "167",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "228",
+        "ok": "228",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "472",
+        "ok": "472",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 6514,
+    "percentage": 99.86202667484287
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 2,
+    "percentage": 0.030660738923808065
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 7,
+    "percentage": 0.10731258623332823
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "2.82",
+        "ok": "2.82",
+        "ko": "-"
+    }
+},
+contents: {
+"req_uc01-1-openroot-663278295": {
+        type: "REQUEST",
+        name: "UC01.1_OpenRootPage_GET_/welcome.pl",
+path: "UC01.1_OpenRootPage_GET_/welcome.pl",
+pathFormatted: "req_uc01-1-openroot-663278295",
+stats: {
+    "name": "UC01.1_OpenRootPage_GET_/welcome.pl",
+    "numberOfRequests": {
+        "total": "932",
+        "ok": "932",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "150",
+        "ok": "150",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "9255",
+        "ok": "9255",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "228",
+        "ok": "228",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "423",
+        "ok": "423",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "173",
+        "ok": "173",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "208",
+        "ok": "208",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "329",
+        "ok": "329",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "587",
+        "ok": "587",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 925,
+    "percentage": 99.2489270386266
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 7,
+    "percentage": 0.7510729613733905
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.4",
+        "ok": "0.4",
+        "ko": "-"
+    }
+}
+    },"req_uc01-2-openroot--1235586027": {
+        type: "REQUEST",
+        name: "UC01.2_OpenRootPage_GET_/nav.pl",
+path: "UC01.2_OpenRootPage_GET_/nav.pl",
+pathFormatted: "req_uc01-2-openroot--1235586027",
+stats: {
+    "name": "UC01.2_OpenRootPage_GET_/nav.pl",
+    "numberOfRequests": {
+        "total": "932",
+        "ok": "932",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "94",
+        "ok": "94",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "637",
+        "ok": "637",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "130",
+        "ok": "130",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "49",
+        "ok": "49",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "114",
+        "ok": "114",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "139",
+        "ok": "139",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "196",
+        "ok": "196",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "425",
+        "ok": "425",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 932,
+    "percentage": 100.0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.4",
+        "ok": "0.4",
+        "ko": "-"
+    }
+}
+    },"req_uc02-login-post--121184650": {
+        type: "REQUEST",
+        name: "UC02_Login_POST_/login.pl",
+path: "UC02_Login_POST_/login.pl",
+pathFormatted: "req_uc02-login-post--121184650",
+stats: {
+    "name": "UC02_Login_POST_/login.pl",
+    "numberOfRequests": {
+        "total": "932",
+        "ok": "932",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "97",
+        "ok": "97",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "797",
+        "ok": "797",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "154",
+        "ok": "154",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "52",
+        "ok": "52",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "137",
+        "ok": "137",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "165",
+        "ok": "165",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "224",
+        "ok": "224",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "309",
+        "ok": "309",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 932,
+    "percentage": 100.0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.4",
+        "ok": "0.4",
+        "ko": "-"
+    }
+}
+    },"req_uc03-getcities--327965707": {
+        type: "REQUEST",
+        name: "UC03_GetCities_GET_/reservations.pl",
+path: "UC03_GetCities_GET_/reservations.pl",
+pathFormatted: "req_uc03-getcities--327965707",
+stats: {
+    "name": "UC03_GetCities_GET_/reservations.pl",
+    "numberOfRequests": {
+        "total": "932",
+        "ok": "932",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "103",
+        "ok": "103",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "516",
+        "ok": "516",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "141",
+        "ok": "141",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "37",
+        "ok": "37",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "128",
+        "ok": "128",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "151",
+        "ok": "151",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "204",
+        "ok": "204",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "257",
+        "ok": "257",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 932,
+    "percentage": 100.0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.4",
+        "ok": "0.4",
+        "ko": "-"
+    }
+}
+    },"req_uc04-selectciti--949867227": {
+        type: "REQUEST",
+        name: "UC04_SelectCities&Dates_POST_/reservations.pl",
+path: "UC04_SelectCities&Dates_POST_/reservations.pl",
+pathFormatted: "req_uc04-selectciti--949867227",
+stats: {
+    "name": "UC04_SelectCities&Dates_POST_/reservations.pl",
+    "numberOfRequests": {
+        "total": "932",
+        "ok": "932",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "101",
+        "ok": "101",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "800",
+        "ok": "800",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "142",
+        "ok": "142",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "66",
+        "ok": "66",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "121",
+        "ok": "121",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "142",
+        "ok": "142",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "212",
+        "ok": "212",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "489",
+        "ok": "489",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 931,
+    "percentage": 99.89270386266095
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 1,
+    "percentage": 0.1072961373390558
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.4",
+        "ok": "0.4",
+        "ko": "-"
+    }
+}
+    },"req_uc05-selectflig-147028012": {
+        type: "REQUEST",
+        name: "UC05_SelectFlight_POST_/reservations.pl",
+path: "UC05_SelectFlight_POST_/reservations.pl",
+pathFormatted: "req_uc05-selectflig-147028012",
+stats: {
+    "name": "UC05_SelectFlight_POST_/reservations.pl",
+    "numberOfRequests": {
+        "total": "932",
+        "ok": "932",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "102",
+        "ok": "102",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "800",
+        "ok": "800",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "141",
+        "ok": "141",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "55",
+        "ok": "55",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "123",
+        "ok": "123",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "144",
+        "ok": "144",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "207",
+        "ok": "207",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "476",
+        "ok": "476",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 931,
+    "percentage": 99.89270386266095
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 1,
+    "percentage": 0.1072961373390558
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.4",
+        "ok": "0.4",
+        "ko": "-"
+    }
+}
+    },"req_uc06-payment-po-1303024975": {
+        type: "REQUEST",
+        name: "UC06_Payment_POST_/reservations.pl",
+path: "UC06_Payment_POST_/reservations.pl",
+pathFormatted: "req_uc06-payment-po-1303024975",
+stats: {
+    "name": "UC06_Payment_POST_/reservations.pl",
+    "numberOfRequests": {
+        "total": "931",
+        "ok": "931",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "102",
+        "ok": "102",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "557",
+        "ok": "557",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "140",
+        "ok": "140",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "46",
+        "ok": "46",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "122",
+        "ok": "122",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "159",
+        "ok": "159",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "208",
+        "ok": "208",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "268",
+        "ok": "268",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 931,
+    "percentage": 100.0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0.0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0.0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.4",
+        "ok": "0.4",
+        "ko": "-"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
